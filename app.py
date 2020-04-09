398130b6ebdf92f6df86a214bda9d2f6845a0215
John Boyle <boylejm@gmail.com>
2020-04-09 03:42:12 +0100

    make saving and loading work....

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 import os
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
+
+
+board_base = {"LEO":50000, "KRN":45000, "MAY":40000, "KAI":35000, "JUN":30000}
 
 @app.route("/")
 def main():
     return jsonify("Johnstein's FancyPants Godot Server!")
 
+@app.route('/reset/')
+def reset():
+    global board
+    board = board_base
+    save_leaderboard()
+    return('Reset Board OK')
+
 @app.route('/leaderboard/')
 def get_leaderboard():
-    global board
-    board = {"LEO":50000, "KRN":45000, "MAY":40000, "KAI":35000, "JUN":30000}
-    print(board)
+    #global board
+    #board = {"LEO":50000, "KRN":45000, "MAY":40000, "KAI":35000, "JUN":30000}
+    #print(board)
     return jsonify(board)
 
 
@@ -25,7 +36,21 @@ def update_leaderboard():
         board[d] = new_stuff[d]
     return ("Update OK")
 
+
+def save_leaderboard():
+    print("saving")
+    with open('leaderboard.json', 'w') as outfile:
+        json.dump(board,outfile)
+
+def load_leaderboard():
+    print("loading")
+    with open('leaderboard.json') as json_file:
+        global board
+        board = json.load(json_file)
+
 if __name__ == "__main__":
+    reset()
+    load_leaderboard()
     app.run()
 
 
