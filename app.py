10735714be6e5b0f2e2dad7281664885c16f09d1
John Boyle <boylejm@gmail.com>
2020-04-09 02:49:28 +0100

    simple leaderboard get/put

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return jsonify("Johnstein's FancyPants Godot Server!")
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/leaderboard/')
+def get_leaderboard():
+    global board
+    board = {"LEO":50000, "KRN":45000, "MAY":40000, "KAI":35000, "JUN":30000}
+    print(board)
+    return jsonify(board)
+
+
+@app.route('/update', methods = ["POST",])
+def update_leaderboard():
+    global board
+    content = request.json
+    new_stuff = content['scores']
+    print(new_stuff)
+    for d in new_stuff:
+        board[d] = new_stuff[d]
+    return ("Update OK")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
+
+
